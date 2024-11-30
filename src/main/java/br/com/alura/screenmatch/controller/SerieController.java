f8781a35578f716b3c9f429f402fe38204d33c57
Patrick Maciel <patrickmaciel.info@gmail.com>
2024-11-29 23:01:47 -0300

    feat: add an endpoint to get serie by id

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,5 +31,10 @@ public class SerieController {
   @GetMapping("/lancamentos")
   public List<SerieDTO> getReleases() {
     return service.findReleases();
+  }
+
+  @GetMapping("/{id}")
+  public SerieDTO getSerieById(@PathVariable Long id) {
+    return service.findById(id);
   }
 }
--- src/main/java/br/com/alura/screenmatch/service/SerieService.java
+++ src/main/java/br/com/alura/screenmatch/service/SerieService.java
@@ -1,9 +1,13 @@
 package br.com.alura.screenmatch.service;
 
 import br.com.alura.screenmatch.dto.SerieDTO;
+import br.com.alura.screenmatch.model.DadosTemporada;
+import br.com.alura.screenmatch.model.Episodio;
 import br.com.alura.screenmatch.model.Serie;
 import br.com.alura.screenmatch.repository.SerieRepository;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -39,5 +43,42 @@ public class SerieService {
   public List<SerieDTO> findReleases() {
 //    return convertData(repository.findTop5ByOrderByEpisodiosDataLancamentoDesc());
     return convertData(repository.buscaSeriesComEpisodiosMaisRecentes());
+  }
+
+  public SerieDTO findById(Long id) {
+    Optional<Serie> serie = repository.findById(id);
+//    Serie dadosSerie = serie.get();
+//    List<DadosTemporada> temporadas = new ArrayList<>();
+//    ConsumoApi consumo = new ConsumoApi();
+//    String ENDERECO = "https://www.omdbapi.com/?t=";
+//    String API_KEY = "&apikey=43e78112";
+//    ConverteDados conversor = new ConverteDados();
+//
+//    for (int i = 1; i <= dadosSerie.getTotalTemporadas(); i++) {
+//      var json = consumo.obterDados(ENDERECO +
+//          dadosSerie.getTitulo().replace(" ", "+") + "&season=" + i + API_KEY);
+//      DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+//      temporadas.add(dadosTemporada);
+//    }
+//    temporadas.forEach(System.out::println);
+//
+//    List<Episodio> episodios = temporadas.stream()
+//        .flatMap(d -> d.episodios().stream()
+//            .map(e -> new Episodio(d.numero(), e)))
+//        .toList();
+//
+//    dadosSerie.setEpisodios(episodios);
+//    repository.save(dadosSerie);
+
+    return serie.map(value -> new SerieDTO(
+        value.getId(),
+        value.getTitulo(),
+        value.getTotalTemporadas(),
+        value.getAvaliacao(),
+        value.getGenero().name(),
+        value.getAtores(),
+        value.getPoster(),
+        value.getSinopse())).orElse(null);
+
   }
 }
